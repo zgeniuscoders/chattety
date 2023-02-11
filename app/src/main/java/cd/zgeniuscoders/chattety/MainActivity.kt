a096fb00b906c3c20798330d1e73281b38d09a04
zgenius coders <zgeniuscoders@gmail.com>
2023-02-11 14:35:26 +0100

    First commit

---
+++ app/src/main/java/cd/zgeniuscoders/chattety/MainActivity.kt
@@ -0,0 +1,11 @@
+package cd.zgeniuscoders.chattety
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/cd/zgeniuscoders/chattety/SlashActivity.kt
@@ -0,0 +1,31 @@
+package cd.zgeniuscoders.chattety
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import cd.zgeniuscoders.chattety.databinding.ActivitySlashBinding
+
+class SlashActivity : AppCompatActivity() {
+
+    private lateinit var handler: Handler
+    private lateinit var binding: ActivitySlashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySlashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+        handler = Handler(Looper.getMainLooper())
+
+        handler.postDelayed({
+            Intent(this, MainActivity::class.java).apply {
+                startActivity(this)
+                finish()
+            }
+        }, 5000)
+    }
+}
