f5df77cd104c3b32ff36cc56cde35238280a4b91
zgenius coders <zgeniuscoders@gmail.com>
2023-02-20 14:35:49 +0100

    Drawer navigation

--- app/src/main/java/cd/zgeniuscoders/chattety/activities/MainActivity.kt
+++ app/src/main/java/cd/zgeniuscoders/chattety/activities/MainActivity.kt
@@ -2,42 +2,65 @@ package cd.zgeniuscoders.chattety.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import cd.zgeniuscoders.chattety.R
 import cd.zgeniuscoders.chattety.databinding.ActivityMainBinding
 import cd.zgeniuscoders.chattety.fragments.AddPostFragment
 import cd.zgeniuscoders.chattety.fragments.HomeFragment
+import com.google.android.material.navigation.NavigationView.OnNavigationItemSelectedListener
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnNavigationItemSelectedListener {
     private lateinit var binding: ActivityMainBinding
+    private var drawerToggle: ActionBarDrawerToggle? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        loadFragment(HomeFragment())
+        drawerToggle =
+            ActionBarDrawerToggle(this, binding.drawerLayout, R.string.open, R.string.close)
+        binding.drawerLayout.addDrawerListener(drawerToggle!!)
+        drawerToggle!!.syncState()
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
-        binding.bottomNavigationView.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.home_fragment -> {
-                    loadFragment(HomeFragment())
-                    return@setOnItemSelectedListener true
-                }
-                R.id.post_fragmemt -> {
-                    loadFragment(AddPostFragment())
-                    return@setOnItemSelectedListener true
-                }
-                else -> {
-                    return@setOnItemSelectedListener false
-                }
+        binding.navigationView.setNavigationItemSelectedListener(this)
+
+
+        val navController = findNavController(R.id.fragment_container)
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.favorite -> {
+
             }
+            R.id.developper -> {
+
+            }
+        }
+        return false
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (drawerToggle!!.onOptionsItemSelected(item)) {
+            true
+        } else super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.close()
         }
     }
 
-    fun loadFragment(myFragment: Fragment) {
-        val fragment = supportFragmentManager.beginTransaction()
-        fragment.replace(R.id.fragment_container, myFragment)
-        fragment.commit()
-    }
 }
